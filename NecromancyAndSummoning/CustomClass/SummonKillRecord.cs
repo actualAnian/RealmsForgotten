a5ae8efb91bdd51a948550ae94ca678df814037a
Cephas369 <pedro-leitao@outlook.com>
2023-07-13 19:51:39 -0400

    Added RFCustomHorses, Necromancy and RFEffects

---
+++ NecromancyAndSummoning/CustomClass/SummonKillRecord.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TaleWorlds.CampaignSystem.Party;
+
+namespace NecromancyAndSummoning.CustomClass
+{
+	// Token: 0x02000014 RID: 20
+	internal class SummonKillRecord
+	{
+		// Token: 0x17000029 RID: 41
+		// (get) Token: 0x060000C0 RID: 192 RVA: 0x00005BA2 File Offset: 0x00003DA2
+		// (set) Token: 0x060000C1 RID: 193 RVA: 0x00005BAA File Offset: 0x00003DAA
+		internal PartyBase party { get; set; }
+
+		// Token: 0x1700002A RID: 42
+		// (get) Token: 0x060000C2 RID: 194 RVA: 0x00005BB3 File Offset: 0x00003DB3
+		// (set) Token: 0x060000C3 RID: 195 RVA: 0x00005BBB File Offset: 0x00003DBB
+		internal string unitId { get; set; }
+
+		// Token: 0x1700002B RID: 43
+		// (get) Token: 0x060000C4 RID: 196 RVA: 0x00005BC4 File Offset: 0x00003DC4
+		// (set) Token: 0x060000C5 RID: 197 RVA: 0x00005BCC File Offset: 0x00003DCC
+		internal int killCount { get; set; }
+
+		// Token: 0x060000C6 RID: 198 RVA: 0x00005BD5 File Offset: 0x00003DD5
+		public SummonKillRecord()
+		{
+			this.party = null;
+			this.unitId = "";
+			this.killCount = 0;
+		}
+
+		// Token: 0x060000C7 RID: 199 RVA: 0x00005BFB File Offset: 0x00003DFB
+		public SummonKillRecord(PartyBase party, string unitId, int killCount)
+		{
+			this.party = party;
+			this.unitId = unitId;
+			this.killCount = killCount;
+		}
+	}
+}
---
+++ RFEffects/AnoritDamageParticleModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using TaleWorlds.Core;
+using TaleWorlds.Engine;
+using TaleWorlds.MountAndBlade;
+
+namespace RFEffects
+{
+	// Token: 0x02000003 RID: 3
+	public class AnoritDamageParticleModel : DefaultDamageParticleModel
+	{
+		// Token: 0x0600000C RID: 12 RVA: 0x000023A4 File Offset: 0x000005A4
+		public AnoritDamageParticleModel()
+		{
+			this._bloodStartHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_blood_sword_enter");
+			this._bloodContinueHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_blood_sword_inside");
+			this._bloodEndHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_blood_sword_exit");
+			this._sweatStartHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_sweat_sword_enter");
+			this._sweatContinueHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_sweat_sword_enter");
+			this._sweatEndHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_sweat_sword_enter");
+			this._missileHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("psys_game_blood_sword_enter");
+			this._fireStartHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("battleground_fire_smoke_square");
+			this._fireMissileHitParticleIndex = ParticleSystemManager.GetRuntimeIdByName("battleground_fire_smoke_square");
+		}
+
+		// Token: 0x0600000D RID: 13 RVA: 0x00002488 File Offset: 0x00000688
+		public override void GetMeleeAttackBloodParticles(Agent attacker, Agent victim, in Blow blow, in AttackCollisionData collisionData, out HitParticleResultData particleResultData)
+		{
+			particleResultData.StartHitParticleIndex = this._bloodStartHitParticleIndex;
+			particleResultData.ContinueHitParticleIndex = this._bloodContinueHitParticleIndex;
+			particleResultData.EndHitParticleIndex = this._bloodEndHitParticleIndex;
+			if (blow.WeaponRecord.WeaponFlags.HasFlag(WeaponFlags.CanKnockDown) && blow.WeaponRecord.WeaponFlags.HasFlag(WeaponFlags.CanHook))
+			{
+				particleResultData.StartHitParticleIndex = this._fireStartHitParticleIndex;
+				this.AddToTheListOfFlame(attacker, victim);
+			}
+		}
+
+		// Token: 0x0600000E RID: 14 RVA: 0x00002515 File Offset: 0x00000715
+		public override void GetMeleeAttackSweatParticles(Agent attacker, Agent victim, in Blow blow, in AttackCollisionData collisionData, out HitParticleResultData particleResultData)
+		{
+			particleResultData.StartHitParticleIndex = this._sweatStartHitParticleIndex;
+			particleResultData.ContinueHitParticleIndex = this._sweatContinueHitParticleIndex;
+			particleResultData.EndHitParticleIndex = this._sweatEndHitParticleIndex;
+		}
+
+		// Token: 0x0600000F RID: 15 RVA: 0x0000253E File Offset: 0x0000073E
+		public override int GetMissileAttackParticle(Agent attacker, Agent victim, in Blow blow, in AttackCollisionData collisionData)
+		{
+			if (blow.WeaponRecord.WeaponFlags.HasFlag(WeaponFlags.AffectsAreaBig))
+			{
+				this.AddToTheListOfFlame(attacker, victim);
+				return this._fireMissileHitParticleIndex;
+			}
+			return this._missileHitParticleIndex;
+		}
+
+		// Token: 0x06000010 RID: 16 RVA: 0x00002578 File Offset: 0x00000778
+		private void AddToTheListOfFlame(Agent attacker, Agent victim)
+		{
+			AnoritMissionBehaviour missionBehavior = Mission.Current.GetMissionBehavior<AnoritMissionBehaviour>();
+			missionBehavior.toBeAdded.Add(victim);
+			if (!missionBehavior.attackerId.ContainsKey(victim.Index))
+			{
+				missionBehavior.attackerId.Add(victim.Index, attacker.Index);
+				return;
+			}
+			missionBehavior.attackerId[victim.Index] = attacker.Index;
+		}
+
+		// Token: 0x04000001 RID: 1
+		private readonly int _bloodStartHitParticleIndex = -1;
+
+		// Token: 0x04000002 RID: 2
+		private readonly int _bloodContinueHitParticleIndex = -1;
+
+		// Token: 0x04000003 RID: 3
+		private readonly int _bloodEndHitParticleIndex = -1;
+
+		// Token: 0x04000004 RID: 4
+		private readonly int _sweatStartHitParticleIndex = -1;
+
+		// Token: 0x04000005 RID: 5
+		private readonly int _sweatContinueHitParticleIndex = -1;
+
+		// Token: 0x04000006 RID: 6
+		private readonly int _sweatEndHitParticleIndex = -1;
+
+		// Token: 0x04000007 RID: 7
+		private readonly int _missileHitParticleIndex = -1;
+
+		// Token: 0x04000008 RID: 8
+		private readonly int _fireStartHitParticleIndex = -1;
+
+		// Token: 0x04000009 RID: 9
+		private readonly int _fireMissileHitParticleIndex = -1;
+	}
+}
