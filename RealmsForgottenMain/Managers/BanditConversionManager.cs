48f4c4191a184301ba9fd5d0300dc0f6dce397af
gupolicarpo <118315424+gupolicarpo@users.noreply.github.com>
2024-07-13 12:14:14 +0200

    add my managers

---
+++ RealmsForgottenMain/Managers/BanditConversionManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaleWorlds.CampaignSystem;
+using RealmsForgotten.Career;
+
+namespace RealmsForgotten.Career
+{
+    public static class BanditConversionManager
+    {
+        public static event EventHandler<BanditConversionEvent> BanditConverted;
+
+        public static void OnBanditConverted(Hero hero, int banditCount)
+        {
+            BanditConverted?.Invoke(null, new BanditConversionEvent(hero, banditCount));
+        }
+    }
+}
