3f6e10003bdb1d3dd7b75e4d614d223b4a9e4d44
gupolicarpo <118315424+gupolicarpo@users.noreply.github.com>
2024-07-13 12:15:05 +0200

    Added my models

---
+++ RealmsForgottenMain/Models/RaceSpeedBonusModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealmsForgotten.Quest.SecondUpdate;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using RealmsForgotten.Quest;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+
+namespace RealmsForgotten.Models
+{
+    public class RaceSpeedBonusModel : DefaultPartySpeedCalculatingModel
+    {
+        public override ExplainedNumber CalculateBaseSpeed(MobileParty party, bool includeDescriptions = false,
+            int additionalTroopOnFootCount = 0, int additionalTroopOnHorseCount = 0)
+        {
+            ExplainedNumber baseValue = base.CalculateBaseSpeed(party, includeDescriptions, additionalTroopOnFootCount, additionalTroopOnHorseCount);
+
+            Hero partyOwner = party.LeaderHero;
+
+            if (partyOwner != null && partyOwner.Culture.StringId == "devils")
+                baseValue.AddFactor(0.20f, new TextObject("{=culture_bonus}Culture Speed Bonus"));
+
+            return baseValue;
+        }
+    }
+}
---
+++ RealmsForgottenMain/Models/ResistanceTuple.cs
@@ -0,0 +1,21 @@
+﻿using SandBox.GameComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealmsForgotten.Models
+{
+    public class ResistanceTuple
+    {
+        public string ResistedDamageType { get; set; }
+        public float ReductionPercent { get; set; }
+
+        public ResistanceTuple(string resistedDamageType, float reductionPercent)
+        {
+            ResistedDamageType = resistedDamageType;
+            ReductionPercent = reductionPercent;
+        }
+    }
+}
