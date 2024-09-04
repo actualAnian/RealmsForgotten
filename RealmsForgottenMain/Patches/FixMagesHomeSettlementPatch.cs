928f56116a6fc1c9e86c901cef32286de45e1ce6
Cephas369 <pedro-leitao@outlook.com>
2024-09-03 21:17:33 -0400

    Fixed mage clan having wrong initial settlement due to a bug with the settlement distances

---
+++ RealmsForgottenMain/Patches/FixMagesHomeSettlementPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace RealmsForgotten.Patches;
+
+[HarmonyPatch(typeof(Clan), "FindSettlementScoreForBeingHomeSettlement")]
+public static class FixMagesHomeSettlementPatch
+{
+    public static void Postfix(Settlement settlement, ref float __result)
+    {
+        if (settlement.Culture?.StringId == "mage")
+            __result = 999999f;
+    }
+}
